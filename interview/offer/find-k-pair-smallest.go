9bee3db96fd1cd5dc81f9cc83bab9dc3b6b79687
michael <tommyhey@163.com>
2024-10-29 20:05:06 +0800

    add k pair smallest offer problem

---
+++ interview/offer/find-k-pair-smallest.go
@@ -0,0 +1,8 @@
+package offer
+
+//TODO:
+// https://leetcode.cn/problems/find-k-pairs-with-smallest-sums/solutions/1208350/cha-zhao-he-zui-xiao-de-kdui-shu-zi-by-l-z526/
+
+func KSmallestPairs(nums1 []int, nums2 []int, k int) [][]int {
+	return nil
+}
